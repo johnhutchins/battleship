fad352fe710da00b4e0d9dce14bffb32c38f2259
John Hutchins <johnwallshutchins@gmail.com>
2020-04-13 20:48:01 -0400

    styling and gameboard

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import './GameBoard.css'
-export default class Trivia extends Component {
+export default class GameBoard extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            boats: []
         }
     }
 
+    createGame = () => {
+        //choose 3 random sets of 2,3,3,4
+        let boats = []
+
+    }
+
+    checkHit = (e) => {
+        console.log('e == ' + e.target.id)
+        //if hit is in one of the num
+    }
+
     createBoard = () => {
-        return [...Array(100)].map((e, i) => <div className="grid-item" key={i}>♦</div>)
+        return [...Array(100)].map((e, i) => <div className="grid-item" id={i} key={i} onClick={this.checkHit}>😈</div>)
     }
 
     render() {
